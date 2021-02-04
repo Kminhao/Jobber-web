fae9c96c48f6d875ed2df035fe2d7e378e34a99e
Gabriel <gabri.elf@hotmail.com>
2021-02-04 20:10:07 -0300

    landing finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './assets/styles/global.css';
-import Header from './components/Header/Header';
+import './assets/styles/global.css'
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <Header />
+    <Landing/>
   );
 }
 
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css'
+
+
+
+function Landing() {
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="navbar">
+                    <a href="">Quem Somos</a>
+                    <a href="">Nossos Serviços</a>
+                    <a href="">Seja um Parceiro de Negócios</a>
+                    <div id="log">
+                        <a href="">Log In </a>
+                        <hr/>
+                        <a href="">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    );
+}
+
+
+export default Landing;
