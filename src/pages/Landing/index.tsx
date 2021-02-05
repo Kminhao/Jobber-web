09b580a98038e7ba8d26dba1fe4421c462dcc48a
Gabriel <gabri.elf@hotmail.com>
2021-02-05 13:37:27 -0300

    layout da landing finalizado

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import './style.css'
-
+import Logo from '../../assets/logo.png';
 
 
 function Landing() {
     return (
         <div className="container">
-            <div className="header">
+            <header className="header">
+                <div className="img">
+                    <a href=""><img src={Logo} alt="" /></a>
+                </div>
                 <div className="navbar">
+
+                    <a href="">Inicio</a>
                     <a href="">Quem Somos</a>
                     <a href="">Nossos Serviços</a>
                     <a href="">Seja um Parceiro de Negócios</a>
                     <div id="log">
-                        <a href="">Log In </a>
-                        <hr/>
-                        <a href="">Sign Up</a>
+                        <a href="">Log In <hr /> Sign Up</a>
+
                     </div>
                 </div>
-            </div>
-        </div>
+            </header>
+            <footer className="footer">
+                <div className="content">
+                    <h2>IJobber Technology</h2>
+                </div>
+            </footer>
 
+        </div>
 
     );
 }
